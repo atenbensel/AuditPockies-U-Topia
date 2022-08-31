330f2bbc9e5877e173f52794266efb021a0e5068
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2022-08-30 22:05:59 -0400

    Add files via upload

---
+++ hardhat.config.ts
@@ -0,0 +1,56 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-abi-exporter";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.7",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 10_000,
+      },
+    },
+  },
+  networks: {
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [process.env.WALLET_PRIVATE_KEY!].filter(Boolean),
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: process.env.MNEMONIC
+        ? { mnemonic: process.env.MNEMONIC }
+        : [process.env.WALLET_PRIVATE_KEY!].filter(Boolean),
+    },
+    hardhat: {
+      initialBaseFeePerGas: 0,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  typechain: {
+    outDir: "./typechain",
+  },
+  abiExporter: {
+    path: "./abis",
+    except: ["IPockies"],
+    only: ["Pockies"],
+  },
+  gasReporter: {
+    enabled: !process.env.CI,
+    currency: "USD",
+    gasPrice: 50,
+    src: "contracts",
+    coinmarketcap: "7643dfc7-a58f-46af-8314-2db32bdd18ba",
+  },
+  // mocha: {
+  //   timeout: 60_000,
+  // },
+};
+
+export default config;
